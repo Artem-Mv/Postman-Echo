83d3900a8130e38ec357e06cbd52618ebcafe797
Artem-Mv <Stimul113@gmail.com>
2022-05-09 18:41:39 +0700

    fix fail

--- src/test/java/PostmanEchoApiTest.java
+++ src/test/java/PostmanEchoApiTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoApiTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("12name"))
+                .body("data", equalTo("name"))
         ;
     }
 }
